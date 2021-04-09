a5fedb2d6f3987de725a95b9033bbb31ffad370f
ryanperretta6 <ryan.perretta@gmail.com>
2021-04-09 03:54:59 -0400

    added getAllFights function to fullCardDistributions

--- data/fullCardDistributions.js
+++ data/fullCardDistributions.js
@@ -1,6 +1,16 @@
-const mongoCollections = require('../config/mongoCollections');
+const mongoCollections = require("../config/mongoCollections");
 const fullCardDistributions = mongoCollections.fullCardDistributions;
+const mongo = require("mongodb");
 
-let exportedMethods = {};
+let exportedMethods = {
+    getAllFights: async () => {
+        const fullCardDistCollection = await fullCardDistributions();
+
+        let data = await fullCardDistCollection.find();
+        for (card in data) card._id = card._id.toString();
+
+        return data;
+    },
+};
 
 module.exports = exportedMethods;
