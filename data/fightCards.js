5d8b3c22a9bec66e8e39b47f04d6cdc6ea20b88d
Luke McEvoy <43505529+luke-mcevoy@users.noreply.github.com>
2021-04-11 17:36:08 -0400

    Update fightCards.js

--- data/fightCards.js
+++ data/fightCards.js
@@ -1,26 +1,5 @@
 const mongoCollections = require("../config/mongoCollections");
 const fightCards = mongoCollections.fightCards;
-<<<<<<< HEAD
-const mongo = require("mongodb");
-const checker = require("../errorChecking");
-
-let exportedMethods = {
-    async getFightCardById(id) {
-        const fightCardsCollection = await fightCards();
-        const fightCard = await fightCardsCollection.findOne({ _id: id });
-        if (!fightCard) throw "Fight card not found";
-        return fightCard;
-    },
-    async addFightCard(newFightCardObject) {
-        // validateFighterObject(newBoutObject);
-        const fightCardsCollection = await fightCards();
-        const newInsertInformation = await fightCardsCollection.insertOne(
-            newFightCardObject
-        );
-        if (newInsertInformation.insertedCount === 0) throw "Insert failed!";
-        return await this.getFightCardById(newInsertInformation.insertedId);
-    },
-=======
 function checkStrings(s) {
   if (!s || typeof s !== "string" || s.trim() === "") return false;
   return true;
@@ -78,7 +57,6 @@ let exportedMethods = {
       newInsertInformation.insertedId
     );
   },
->>>>>>> elias
 };
 
 module.exports = exportedMethods;
