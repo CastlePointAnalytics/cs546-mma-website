17cb7a012e1afba06e97fb4ddab78073b7649e2d
Elias <>
2021-04-11 15:39:17 -0400

    Error checking implementated when creating new fightCard

--- data/fightCards.js
+++ data/fightCards.js
@@ -1,7 +1,23 @@
 const mongoCollections = require("../config/mongoCollections");
 const fightCards = mongoCollections.fightCards;
-const fighters = require("./fighters");
 const boutOdds = require("./boutOdds");
+function checkStrings(s) {
+  if (!s || typeof s !== "string" || s.trim() === "") return false;
+  return true;
+}
+function checkDate(dt) {
+  //Note: Code gotten from user Elian Ebbing on stackoverflow https://stackoverflow.com/questions/6177975/how-to-validate-date-with-format-mm-dd-yyyy-in-javascript
+  if (!/^\d{1,2}\/\d{1,2}\/\d{4}$/.test(dt)) return false;
+  let parts = dt.split("/");
+  let day = parseInt(parts[1], 10);
+  let month = parseInt(parts[0], 10);
+  let year = parseInt(parts[2], 10);
+  if (month == 0 || month > 12) return false;
+  let monthLength = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+  if (year % 400 == 0 || (year % 100 != 0 && year % 4 == 0))
+    monthLength[1] = 29;
+  return day > 0 && day <= monthLength[month - 1];
+}
 function validateFightCard(fightCard) {
   // {
   //     "_id": "507f1f77bcf86cd799439011",
@@ -10,36 +26,15 @@ function validateFightCard(fightCard) {
   //     "date": "10/15/2021",
   //     "title": "UFC 260"
   // }
-  // {
-  //     "_id": "807f191a810c19729de860ae",
-  //     "fighter1": {
-  //         "_id": "507f1f77bcf86cd799439011",
-  //         "firstName": "Jon",
-  //         "lastName": "Jones",
-  //         "record": { "wins": 26, "losses": 1, "draws": 0 },
-  //         "age": 33,
-  //         "height": 193,
-  //         "weight": 93,
-  //         "reach": 214.63,
-  //         "location": "USA"
-  //     },
-  //     "fighter2": {
-  //         "_id": "757f191a810c19729de860ae",
-  //         "firstName": "Khabib",
-  //         "lastName": "Nurmagomedov",
-  //         "record": { "wins": 29, "losses": 0, "draws": 0 },
-  //         "age": 32,
-  //         "height": 177,
-  //         "weight": 70,
-  //         "reach": 70,
-  //         "location": "Russia"
-  //     },
-  //     "cpaProb": { "fighter1": 43.2, "fighter2": 56.8 },
-  //     "vegasProb": { "fighter1": 48.1, "fighter2": 51.9 },
-  //     "vegasMoneyLine": { "fighter1": -570, "fighter2": 65 },
-  //     "expectedValue": { "fighter1": 1.7, "fighter2": 6.2 },
-  //     "fightDate": "4/10/2021"
-  // }
+  if (!checkStrings(fightCard.location)) {
+    throw `Fight Card Location must be a non empty string, it is currenty: ${fightCard.location}`;
+  }
+  if (!checkDate(fightCard.date)) {
+    throw `Date must be formatted in mm/dd/yyyy, it is currently: ${fightCard.date}`;
+  }
+  if (!checkStrings(fightCard.title)) {
+    throw `Fight Card Title must be a non empty string, it is currenty: ${fightCard.title}`;
+  }
   for (let i = 0; i < fightCard.allBoutOdds.length; i++) {
     //Loop through allBoutOdds and confirming all the boutOdds are formatted correctly
     boutOdds.validateBoutObject(fightCard.allBoutOdds[i]); //Method in ./boutOdds.js
@@ -54,7 +49,7 @@ let exportedMethods = {
     return fightCard;
   },
   async addFightCard(newFightCardObject) {
-    validateFighterObject(newFightCardObject);
+    validateFightCard(newFightCardObject);
     const fightCardsCollection = await fightCards();
     const newInsertInformation = await fightCardsCollection.insertOne(
       newFightCardObject
