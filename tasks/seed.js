5cf5c6a0b4fd9a8fd4772a2c755a0c556cc99215
Elias <>
2021-04-23 12:41:48 -0400

    cleaning merge conflict

--- tasks/seed.js
+++ tasks/seed.js
@@ -1,9 +1,6 @@
 const dbConnection = require("../config/mongoConnection");
 const data = require("../data");
 const fetchData = require("../fetchData/fetchData");
-const dbConnection = require("../config/mongoConnection");
-const data = require("../data");
-const fetchData = require("../fetchData/fetchData");
 const fightersCollection = data.fighters;
 const fightCardsCollection = data.fightCards;
 const boutOddsCollection = data.boutOdds;
@@ -16,28 +13,6 @@ async function main() {
   const db = await dbConnection();
   await db.dropDatabase();
 
-  const fightersJSONData = await fetchData.getFighters();
-  const fightCardsJSONData = await fetchData.getFightCards();
-  const boutOddsJSONData = await fetchData.getBoutOdds();
-  const currFightCard = await fightCardsCollection.addFightCard(
-    fightCardsJSONData[0]
-  );
-  let boutIndex = 0;
-  for (let i = 0; i < fightersJSONData.length; i += 2) {
-    currBoutOdd = boutOddsJSONData[boutIndex++];
-    const odds = {
-      fighter1: (await fightersCollection.addFighter(fightersJSONData[i]))._id,
-      fighter2: (await fightersCollection.addFighter(fightersJSONData[i + 1]))
-        ._id,
-      cpaProb: currBoutOdd.cpaProb,
-      vegasProb: currBoutOdd.vegasProb,
-      vegasMoneyLine: currBoutOdd.vegasMoneyLine,
-      expectedValue: currBoutOdd.expectedValue,
-      fightDate: currFightCard.date,
-    };
-    await boutOddsCollection.addBout(currFightCard._id, odds);
-  }
-  await db.serverConfig.close();
   const fightersJSONData = await fetchData.getFighters();
   const fightCardsJSONData = await fetchData.getFightCards();
   const boutOddsJSONData = await fetchData.getBoutOdds();
