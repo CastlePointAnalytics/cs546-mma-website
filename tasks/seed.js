9cd5d98fff756d51dc15878d535fc794d442861e
Elias <>
2021-04-23 12:07:14 -0400

    fixed merge conflict

--- tasks/seed.js
+++ tasks/seed.js
@@ -1,12 +1,9 @@
-<<<<<<< HEAD
 const dbConnection = require("../config/mongoConnection");
 const data = require("../data");
 const fetchData = require("../fetchData/fetchData");
-=======
-const dbConnection = require('../config/mongoConnection');
-const data = require('../data');
-const fetchData = require('../fetchData/fetchData');
->>>>>>> develop
+const dbConnection = require("../config/mongoConnection");
+const data = require("../data");
+const fetchData = require("../fetchData/fetchData");
 const fightersCollection = data.fighters;
 const fightCardsCollection = data.fightCards;
 const boutOddsCollection = data.boutOdds;
@@ -19,7 +16,6 @@ async function main() {
   const db = await dbConnection();
   await db.dropDatabase();
 
-<<<<<<< HEAD
   const fightersJSONData = await fetchData.getFighters();
   const fightCardsJSONData = await fetchData.getFightCards();
   const boutOddsJSONData = await fetchData.getBoutOdds();
@@ -42,30 +38,28 @@ async function main() {
     await boutOddsCollection.addBout(currFightCard._id, odds);
   }
   await db.serverConfig.close();
-=======
-	const fightersJSONData = await fetchData.getFighters();
-	const fightCardsJSONData = await fetchData.getFightCards();
-	const boutOddsJSONData = await fetchData.getBoutOdds();
-	const currFightCard = await fightCardsCollection.addFightCard(
-		fightCardsJSONData[0],
-	);
-	let boutIndex = 0;
-	for (let i = 0; i < fightersJSONData.length; i += 2) {
-		currBoutOdd = boutOddsJSONData[boutIndex++];
-		const odds = {
-			fighter1: (await fightersCollection.addFighter(fightersJSONData[i]))._id,
-			fighter2: (await fightersCollection.addFighter(fightersJSONData[i + 1]))
-				._id,
-			cpaProb: currBoutOdd.cpaProb,
-			vegasProb: currBoutOdd.vegasProb,
-			vegasMoneyLine: currBoutOdd.vegasMoneyLine,
-			expectedValue: currBoutOdd.expectedValue,
-			fightDate: currFightCard.date,
-		};
-		await boutOddsCollection.addBout(currFightCard._id, odds);
-	}
-	await db.serverConfig.close();
->>>>>>> develop
+  const fightersJSONData = await fetchData.getFighters();
+  const fightCardsJSONData = await fetchData.getFightCards();
+  const boutOddsJSONData = await fetchData.getBoutOdds();
+  const currFightCard = await fightCardsCollection.addFightCard(
+    fightCardsJSONData[0]
+  );
+  let boutIndex = 0;
+  for (let i = 0; i < fightersJSONData.length; i += 2) {
+    currBoutOdd = boutOddsJSONData[boutIndex++];
+    const odds = {
+      fighter1: (await fightersCollection.addFighter(fightersJSONData[i]))._id,
+      fighter2: (await fightersCollection.addFighter(fightersJSONData[i + 1]))
+        ._id,
+      cpaProb: currBoutOdd.cpaProb,
+      vegasProb: currBoutOdd.vegasProb,
+      vegasMoneyLine: currBoutOdd.vegasMoneyLine,
+      expectedValue: currBoutOdd.expectedValue,
+      fightDate: currFightCard.date,
+    };
+    await boutOddsCollection.addBout(currFightCard._id, odds);
+  }
+  await db.serverConfig.close();
 }
 
 main();
