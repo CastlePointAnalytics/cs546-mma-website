91107de52060d08fc8d6a12adee8faec72575267
ryanperretta6 <ryan.perretta@gmail.com>
2021-04-25 11:08:31 -0400

    added client side form checking

--- public/js/validateMatchUp.js
+++ public/js/validateMatchUp.js
@@ -1,18 +1,26 @@
 let matchUpForm = document.getElementById("matchUpForm");
 let fighter1 = document.getElementById("fighter1");
 let fighter2 = document.getElementById("fighter2");
+let fighter1Error = document.getElementById("fighter1Error");
+let fighter2Error = document.getElementById("fighter2Error");
+let sameFighterError = document.getElementById("sameFighterError");
 
 if (matchUpForm) {
     matchUpForm.addEventListener("submit", (event) => {
-        event.preventDefault();
         if (fighter1.value == "Choose Fighter") {
             // display error that you must choose a fighter
+            event.preventDefault();
+            fighter1Error.hidden = false;
         } else {
+            fighter1Error.hidden = true;
         }
 
         if (fighter2.value == "Choose Fighter") {
             // display error that you must choose a fighter
+            event.preventDefault();
+            fighter2Error.hidden = false;
         } else {
+            fighter2Error.hidden = true;
         }
 
         if (
@@ -20,7 +28,10 @@ if (matchUpForm) {
             fighter1.value == fighter2.value
         ) {
             // display error that you cannot choose the same fighter
+            event.preventDefault();
+            sameFighterError.hidden = false;
         } else {
+            sameFighterError.hidden = true;
         }
     });
 }
