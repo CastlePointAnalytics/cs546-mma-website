f57c913cdac659d3a1066101307667ef7a740a66
lmcevoy <lmcevoy@stevens.edu>
2021-05-04 14:30:47 -0400

    login cookie authentication

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,30 @@ const data = require('../data');
 const userData = data.users;
 const errorChecking = require('../errorChecking');
 const er = errorChecking.checker;
+const bcrypt = require('bcryptjs');
+
+function validateFormData(inputUsername, inputPassword) {
+	if (typeof inputUsername !== 'string' || !inputUsername.trim()) {
+		throw 'Invalid username';
+	}
+	if (typeof inputPassword !== 'string' || !inputPassword.trim()) {
+		throw 'Invalid password';
+	}
+}
+
+async function authenticatedUser(inputUsername, inputPassword) {
+	validateFormData(inputUsername, inputPassword);
+	let presentUser = false;
+	for (let user of usersData) {
+		if (
+			user.username.toLowerCase() == inputUsername &&
+			(await bcrypt.compare(inputPassword, user.hashedPassword))
+		) {
+			presentUser = true;
+		}
+	}
+	return presentUser;
+}
 
 router.get('/', async (req, res) => {
 	res.render('user/login');
@@ -14,6 +38,35 @@ router.get('/', async (req, res) => {
 	// 	// render a view with a login form
 	// 	res.render('user/login');
 	// }
+});
+
+router.post('/login', async (req, res) => {
+	// if successful username + password
+	if (await authenticatedUser(req.body.username, req.body.password)) {
+		// set AuthCookie
+		let currUser;
+		for (let user of await userData.getAllUsers()) {
+			if (
+				user.username.toLowerCase() == req.body.username &&
+				(await bcrypt.compare(req.body.password, user.hashedPassword))
+			) {
+				currUser = user;
+			}
+		}
+		req.session.user = {
+			id: currUser._id,
+			username: currUser.username,
+			firstName: currUser.firstName,
+			lastName: currUser.lastName,
+			profession: currUser.profession,
+			bio: currUser.bio,
+		};
+		res.redirect('/profile');
+	} else {
+		// if not successful
+		res.status(401).render('user/login');
+		// error message with HTTP 401
+	}
 });
 
 router.get('/:id', async (req, res) => {
