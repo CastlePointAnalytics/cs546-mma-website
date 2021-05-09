96c7a3de3a7f36e4cce308fea0e0157f88848103
Elias <>
2021-05-09 16:28:01 -0400

    Pickems html is valid and now we are going to pass in arrays of fighters by their ID to the route

--- public/js/fightcards/fightcards.js
+++ public/js/fightcards/fightcards.js
@@ -1,25 +1,30 @@
 $(document).ready(() => {
-  $("#pickemForm").submit(function (event) {
-    //Get name of fightcard
+  $(".pickemForm").submit(function (event) {
     event.preventDefault();
-    let fightCardTitle = $("#title").text();
-    //Get fightcard id
-    let fighter = "";
-    $("input[type='radio']:checked").each(function () {
-      if ($(this).is(":checked")) {
-        fighter = $(this).parent().text().trim();
-      }
-    });
-    console.log(fighter);
+    let id = $(this).attr("id");
+    console.log(id);
+    let parent = $(this);
+    //Get name of fightcard
 
-    $.ajax({
-      url: "/user/updatePickem",
-      method: "POST",
-      contentType: "application/json",
-      data: JSON.stringify({
-        title: fightCardTitle,
-        name: fighter,
-      }),
+    let fightCardTitle = $("#title").text();
+    //We get all the fighter ids that are checked
+    let fighters = [];
+    $("input[type='radio']:checked").each(function () {
+      // console.log(id);
+      // console.log($(this).attr("id"));
+      fighters.push($(this).attr("id").slice(0, -7));
     });
+    console.log(fighters);
+    //console.log(fighter);
+
+    // $.ajax({
+    //   url: "/user/updatePickem",
+    //   method: "POST",
+    //   contentType: "application/json",
+    //   data: JSON.stringify({
+    //     title: fightCardTitle,
+    //     name: fighter,
+    //   }),
+    // });
   });
 });
